24f8a8a3e1a93d4da9f88ba69fdf9e494d668455
Bohdan Benovskyi <bohdanben@interlink-ua.com>
2018-11-08 17:52:43 +0200

    Change options to `data-rjs` and add automatic addition retina.js to precompile.

--- lib/retinajs-rails.rb
+++ lib/retinajs-rails.rb
@@ -1,13 +1,15 @@
 module RetinaJs
   class Engine < ::Rails::Engine
-    config.assets.precompile += %w(retina.js retina.less retina.scss)
+    initializer 'retinajs-rails.assets.precompile' do |app|
+      app.config.assets.precompile += %w( retina.js )
+    end
   end
 end
 
 module ImageHelper
   def image_tag_with_at2x(name_at_1x, options={})
     name_at_2x = name_at_1x.gsub(%r{\.\w+$}, '@2x\0')
-    image_tag(name_at_1x, options.merge("data-at2x" => asset_path(name_at_2x)))
+    image_tag(name_at_1x, options.merge("data-rjs" => asset_path(name_at_2x)))
   end
 end
 ActionView::Base.send :include, ImageHelper
